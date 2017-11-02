4240e94b679f414acefb0835f1fd4005a04367de
Sean Holcroft <info@seanholcroft.co.uk>
2017-11-02 20:19:56 +0000

    Refactor to store items in basket, calculate total only when requested, not updated after each scan. Should allow for special prices.

--- CheckoutKata/CheckoutKata.UnitTests/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata.UnitTests/CheckoutTests.cs
@@ -103,5 +103,25 @@ namespace CheckoutKata.UnitTests
 
             Assert.That(result, Is.EqualTo(185));
         }
+
+        [Test]
+        public void TotalPrice_Correct_If_Three_A_Scanned()
+        {
+            var items = new[]
+            {
+                "A",
+                "A",
+                "A"
+            };
+
+            foreach (var item in items)
+            {
+                _sut.Scan(item);
+            }
+
+            var result = _sut.GetTotalPrice();
+
+            Assert.That(result, Is.EqualTo(130));
+        }
     }
 }
--- CheckoutKata/CheckoutKata/Checkout.cs
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -1,36 +1,40 @@
 ﻿using CheckoutKata.Interfaces;
+using System.Collections.Generic;
 
 namespace CheckoutKata
 {
     public class Checkout : ICheckout
     {
+        private readonly ICollection<string> _basket = new List<string>();
         private int _totalPrice;
 
-        public void Scan(string item)
-        {
-            if (item == "A")
-            {
-                _totalPrice += 50;
-            }
-
-            if (item == "B")
-            {
-                _totalPrice += 30;
-            }
-
-            if (item == "C")
-            {
-                _totalPrice += 20;
-            }
-
-            if (item == "D")
-            {
-                _totalPrice += 15;
-            }
-        }
+        public void Scan(string item) => _basket.Add(item);
 
         public int GetTotalPrice()
         {
+            foreach (var item in _basket)
+            {
+                if (item == "A")
+                {
+                    _totalPrice += 50;
+                }
+
+                if (item == "B")
+                {
+                    _totalPrice += 30;
+                }
+
+                if (item == "C")
+                {
+                    _totalPrice += 20;
+                }
+
+                if (item == "D")
+                {
+                    _totalPrice += 15;
+                }
+            }
+
             return _totalPrice;
         }
     }
