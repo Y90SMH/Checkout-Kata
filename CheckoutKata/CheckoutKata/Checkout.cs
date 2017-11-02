3337a3c31f1b39b140b99c9ddf40941bd768e5e9
Sean Holcroft <info@seanholcroft.co.uk>
2017-11-02 19:30:13 +0000

    Create Checkout.

---
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -0,0 +1,17 @@
+﻿using CheckoutKata.Interfaces;
+
+namespace CheckoutKata
+{
+    public class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            return 0;
+        }
+    }
+}
